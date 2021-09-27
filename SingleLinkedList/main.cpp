759fbd355e867397372fb38a4e351f0a4f941dfe
kmyazrf <91484832+kmyazrf@users.noreply.github.com>
2021-09-27 23:37:50 +0330

    Update main.cpp

--- SingleLinkedList/main.cpp
+++ SingleLinkedList/main.cpp
@@ -2,5 +2,15 @@
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
+    class linklis
+    {
+        public:
+        linklist;
+        ~linklist;
+        void addnode();
+        void delnode(int &);
+        void display;
+    };    
+        
     return 0;
 }
