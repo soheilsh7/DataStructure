2e0e986520fd66b5c55348cf3c6957746f13bf3d
soheilsh7 <soheilshabani.79.ss@gmail.com>
2021-09-27 21:00:07 +0330

    First commit :)

---
+++ SingleLinkedList/cmake-build-debug/CMakeFiles/SingleLinkedList.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SingleLinkedList.dir/main.cpp.o"
+  "SingleLinkedList"
+  "SingleLinkedList.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SingleLinkedList.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
